29ef7f826da4f989badda57a2e1dba1a7609f9b5
Jamie Winsor <jamie@enmasse.com>
2011-10-31 22:22:21 -0700

    Remove unused generator
    we now use Rails 3.1's built in support to generate migrations

--- lib/generators/seo_meta_generator.rb
+++
@@ -1,9 +0,0 @@
-require 'refinerycms-generators'
-require 'refinery/generators'
-
-class SeoMetaGenerator < ::Refinery::Generators::EngineInstaller
-
-  source_root File.expand_path('../../../', __FILE__)
-  engine_name "seo_meta"
-
-end
