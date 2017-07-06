0ac76ecbe1f0441e78cb56cca9de93aeb3e03fe6
sjoulbak <remco_meinen@hotmail.com>
2017-07-06 15:06:43 +0200

    Replace rspec should with expect
    Should will be deprecated, expect is the new way to assert with
    rspec.

--- spec/models/seo_meta_spec.rb
+++ spec/models/seo_meta_spec.rb
@@ -14,23 +14,23 @@ module SeoMeta
 
     context 'responds to' do
       it 'meta_description' do
-        dummy_for_spec.should respond_to(:meta_description)
+        expect(dummy_for_spec).to respond_to(:meta_description)
       end
 
       it 'browser_title' do
-        dummy_for_spec.should respond_to(:browser_title)
+        expect(dummy_for_spec).to respond_to(:browser_title)
       end
     end
 
     context 'allows us to assign to' do
       it 'meta_description' do
         dummy_for_spec.meta_description = 'This is my description of the dummy_for_spec for search results.'
-        dummy_for_spec.meta_description.should == 'This is my description of the dummy_for_spec for search results.'
+        expect(dummy_for_spec.meta_description).to eq('This is my description of the dummy_for_spec for search results.')
       end
 
       it 'browser_title' do
         dummy_for_spec.browser_title = 'An awesome browser title for SEO'
-        dummy_for_spec.browser_title.should == 'An awesome browser title for SEO'
+        expect(dummy_for_spec.browser_title).to eq('An awesome browser title for SEO')
       end
     end
 
@@ -40,7 +40,7 @@ module SeoMeta
         dummy_for_spec.save
 
         dummy_for_spec.reload
-        dummy_for_spec.meta_description.should == 'This is my description of the dummy_for_spec for search results.'
+        expect(dummy_for_spec.meta_description).to eq('This is my description of the dummy_for_spec for search results.')
       end
 
       it 'browser_title' do
@@ -48,7 +48,7 @@ module SeoMeta
         dummy_for_spec.save
 
         dummy_for_spec.reload
-        dummy_for_spec.browser_title.should == 'An awesome browser title for SEO'
+        expect(dummy_for_spec.browser_title).to eq('An awesome browser title for SEO')
       end
     end
 
